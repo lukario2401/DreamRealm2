8308c0a712837b5cceb31d42b29eb24cb8485a6f
lukario <100782878+lukario2401@users.noreply.github.com>
2025-05-30 22:59:26 +0400

    working on medkit

--- src/main/java/me/lukario/dreamRealm2/items/guns_and_crates/Medkit.java
+++ src/main/java/me/lukario/dreamRealm2/items/guns_and_crates/Medkit.java
@@ -12,8 +12,13 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.*;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.*;
 
@@ -132,39 +137,72 @@ public void onClick(InventoryClickEvent e) {
     // perform the slot’s ability
     switch (slot) {
         case 0:
-            player.sendMessage("0");
             break;
         case 1:
             player.sendMessage("1");
+            player.getInventory().addItem(new ItemStack(Material.COOKED_BEEF, 4));
+
             break;
         case 2:
             player.sendMessage("2");
+            player.getInventory().addItem(new ItemStack(Material.MILK_BUCKET));
+
             break;
         case 3:
             player.sendMessage("3");
+            player.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH,120,1,false,false));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,120,1,false,false));
+
             break;
         case 4:
             player.sendMessage("4");
+            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,120,1,false,false));
+
             break;
         case 5:
             player.sendMessage("5");
+            player.addPotionEffect(new PotionEffect(PotionEffectType.INSTANT_HEALTH,5,2,false,false));
+
             break;
         case 6:
-            player.sendMessage("6");
+            player.addPotionEffect(new PotionEffect(PotionEffectType.STRENGTH,600,1,false,false));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,600,1,false,false));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.HASTE,600,1,false,false));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING,600,1,false,false));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,600,1,false,false));
+
+            new BukkitRunnable(){
+                @Override
+                public void run(){
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS,1200,1,false,false));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.SLOWNESS,1200,1,false,false));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.MINING_FATIGUE,1200,1,false,false));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.NAUSEA,1200,1,false,false));
+
+                }
+            }.runTaskTimer(plugin,600,0);
             break;
         case 7:
             player.sendMessage("7");
+
+            ItemStack potion = new ItemStack(Material.LINGERING_POTION);
+            PotionMeta meta = (PotionMeta) potion.getItemMeta();
+
+            meta.addCustomEffect(new PotionEffect(PotionEffectType.REGENERATION, 200, 1), true);
+
+            potion.setItemMeta(meta);
+
+            player.getInventory().addItem(potion);
+
             break;
         case 8:
-            player.sendMessage("8");
             break;
         default:
-            player.sendMessage(ChatColor.GRAY + "Unknown slot!");
             return;
     }
 
     // feedback
-    player.sendMessage(ChatColor.YELLOW + "Activated ability " + (slot + 1));
+//    player.sendMessage(ChatColor.YELLOW + "Activated ability " + (slot + 1));
 
     // disable the button visually
     ItemStack disabled = new ItemStack(Material.BARRIER);
@@ -174,10 +212,7 @@ public void onClick(InventoryClickEvent e) {
     e.getInventory().setItem(slot, disabled);
 }
 
-    private void shortAbilityOne(Player player) {
-        player.sendMessage(ChatColor.AQUA + "Healing boost!");
-        player.setHealth(Math.min(player.getMaxHealth(), player.getHealth() + 4));
-    }
+
 
     /** Checks display name + lore + material */
     private boolean isCorrectItem(ItemStack item) {
